8520a7e92b70fdc3ad61a88997d685e5c24bd161
lilahgit <sitikholilah@hotmail.com>
2025-11-29 22:26:23 +0700

    challenge 5 done

--- src/challenge1.ts
+++ src/challenge1.ts
@@ -1,2 +1,52 @@
+interface BookInterface{
+    title: string;
+    author: string;
+    publicationYear: number;
+}
+
+let books: BookInterface[] = []
+
+function addBook(title:string, author:string, publicationYear:number): void {
+    const newBook: BookInterface = { title, author, publicationYear };
+    books.push(newBook);
+    console.log(`Book added: "${title}" by ${author} (${publicationYear})`);
+}
+
+function listBooks(): void {
+    console.log("All Books:");
+    
+    if (books.length === 0) {
+        console.log("No books available.");
+        return;
+    }
+    books.forEach(book => {
+        console.log(`- ${book.title} by ${book.author} (${book.publicationYear})`);
+    });
+} 
+// menggunakan map
+
+function searchBook(title?: string): void {
+    if (!title) {
+        console.log('Please provide a title to search.');
+        return;
+    }
+
+    const results = books.filter(book => book.title.includes(title)); 
+
+    console.log(`Search Results for "${title}":`);
+    
+    if (results.length === 0) {
+        console.log(`No books found with title containing "${title}".`);
+    } else {
+        results.forEach(book => {
+            console.log(`- ${book.title} by ${book.author} (${book.publicationYear})`);
+        });
+    }
+}
+
+// Adding some books
+addBook('The Great Gatsby', 'F. Scott Fitzgerald', 1925);
+addBook('1984', 'George Orwell', 1949);
+
 // Don't delete code bellow and this code must be at the bottom of the file
 export { addBook, listBooks, searchBook };
